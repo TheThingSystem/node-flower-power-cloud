f06c3f735f8a967e50cb2dbefba2e7448d71e366
Jerome Bouvard <jerome.bouvard@parrot.com>
2015-03-17 16:19:16 +0100

    Add Upload to Garden Function

--- flower-power-cloud.js
+++ flower-power-cloud.js
@@ -7,6 +7,7 @@ var https       = require('https')
   , querystring = require('querystring')
   , url         = require('url')
   , util        = require('util')
+  , request     = require('request')
   ;
 
 
@@ -108,8 +109,8 @@ CloudAPI.prototype._refresh = function(self, callback) {
 CloudAPI.prototype.getGarden = function(callback) {
   var self = this;
 
-  return self.invoke('GET', '/sensor_data/v2/sync?include_s3_urls=1', function(err, code, results) {
-    var count, i, location, locations, sensor, sensors;
+  return self.invoke('GET', '/sensor_data/v3/sync?include_s3_urls=1', function(err, code, results) {
+    var count, i, location, locations, sensor, sensors, user_config_version;
 
     if (!!err) return callback(err);
 
@@ -118,12 +119,12 @@ CloudAPI.prototype.getGarden = function(callback) {
           if (!!err) self.logger.error('invoke', { event: 'sync', diagnostic: err.message });
         else locations[id].samples = results.samples;
 
-        if (--count === 0) callback(null, locations, sensors);
+        if (--count === 0) callback(null, locations, sensors, user_config_version);
       };
     };
 
     count = 0;
-
+    user_config_version = results.user_config_version;
 
     sensors = {};
     for (i = 0; i < results.sensors.length; i++) {
@@ -142,7 +143,7 @@ CloudAPI.prototype.getGarden = function(callback) {
     }
 
     count++;
-    self.roundtrip('GET', '/sensor_data/v1/garden_locations_status', function(err, results) {
+    self.roundtrip('GET', '/sensor_data/v4/garden_locations_status', function(err, results) {
       var i, id;
 
       if (!!err) self.logger.error('invoke', { event: 'garden_locations_status', diagnostic: err.message });
@@ -151,14 +152,73 @@ CloudAPI.prototype.getGarden = function(callback) {
           location = results.locations[i];
           id = location.location_identifier;
           delete(location.location_identifier);
-
+          for (i = 0; i < results.sensors.length; i++) {
+            sensor = results.sensors[i];
+          }
           locations[id].status = location;
         }          
       }
 
-      if (--count === 0) callback(null, locations, sensors);
+      if (--count === 0) callback(null, locations, sensors, user_config_version);
     });
   });
+};
+
+CloudAPI.prototype.uploadGarden = function (sensor_serial, user_config_version, sUpTime, history, currentID, sessionPeriod, sessionStartIndex, callback) {
+
+var self = this;
+var json1 =  {
+            client_datetime_utc : new Date(),
+            user_config_version : user_config_version,
+            tmz_offset : (new Date()).getTimezoneOffset(),
+            
+            session_histories : [{
+               sensor_serial : sensor_serial,
+               session_id : currentID,
+               sample_measure_period : sessionPeriod,
+               sensor_startup_timestamp_utc : sUpTime,
+               session_start_index : sessionStartIndex
+            }],
+            
+            uploads : [{
+               sensor_serial : sensor_serial,
+               upload_timestamp_utc : new Date(),
+               buffer_base64 : history,
+               app_version : "",
+               sensor_fw_version : "",
+               sensor_hw_identifier : "",
+            }]
+           };
+           
+      request({
+         proxy: undefined,
+         method: 'PUT',
+         uri: 'https://apiflowerpower.parrot.com/sensor_data/v5/sample',
+         headers: {Authorization : "Bearer " + self.oauth.access_token},
+         body: (json1 !== null)? JSON.stringify(json1) : undefined
+      }, function (error, response, body) {
+         if (error !== null) {
+            callback(error, null);
+         } else if (response.statusCode !== 200) {
+            var htmlError = self.manageHtmlErrors(response);
+            if (typeof (callback) === 'function') {
+               callback(htmlError, body);
+            }
+         } else {
+            body = JSON.parse(body);
+            if((body.hasOwnProperty("errors") && (body.errors.length > 0))){
+               callback(body.errors[0], null);  
+            }
+            else {
+               if (self.formatter instanceof Function){
+                  body = self.formatter(body);
+               }
+               if (callback instanceof Function) {
+                  callback(null, body);
+               }
+            }
+         }
+      });
 };
 
 
